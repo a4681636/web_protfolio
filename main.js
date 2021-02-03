a1a7df51813cf3d8c0b44ebdd84856fe2b943676
a4681636 <f49427362000@yahoo.com.tw>
2021-02-03 21:33:19 +0800

    FINAL

--- main.js
+++ main.js
@@ -6,6 +6,28 @@ $("#banner").slick({
     });
     
 $("#works").slick({
-        slidesToShow:3,
-        slidesToScroll:2
+        slidesToShow:4,
+        slidesToScroll:2,
+        // 響應式RWD
+        responsive: [
+          {
+          // 平板:小於 1025
+          breakpoint: 1025,
+          settings: {
+            slidesToShow:3,
+            slidesToScroll:1
+          }
+        },
+        // 手機 小於 767
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow:1,
+            slidesToScroll:1
+          }
+        }
+      
+    ]
+
+
       });
